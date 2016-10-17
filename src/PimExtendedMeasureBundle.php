f3d4bf95549afb7e1e431ef5f0f5180d74ff298c
Romain Monceau <romain@akeneo.com>
2016-10-17 11:06:55 +0200

    Add missing measures

---
+++ src/PimExtendedMeasureBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pim\Bundle\ExtendedMeasureBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Romain Monceau <romain@akeneo.com>
+ */
+class PimExtendedMeasureBundle extends Bundle
+{
+}
