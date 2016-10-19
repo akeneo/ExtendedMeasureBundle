c0c47785cb3c9aeec196cea338b3c36ad77189df
jean-marie leroux <jmleroux.pro@gmail.com>
2016-10-19 14:55:57 +0200

    Better unresolvable units (#4)
    Identify unresolvable units

--- src/Exception/DuplicateUnitException.php
+++ src/Exception/DuplicateUnitException.php
@@ -3,8 +3,11 @@
 namespace Pim\Bundle\ExtendedMeasureBundle\Exception;
 
 /**
+ * Exception thown when checking the unicity of a unit inside a family.
+ *
  * @author JM Leroux <jean-marie.leroux@akeneo.com>
  */
 class DuplicateUnitException extends \RuntimeException
 {
+    protected $message = 'A unit cannot belong to more than one measure in the same family.';
 }
--- src/Mapper/MeasureMapper.php
+++ src/Mapper/MeasureMapper.php
@@ -2,8 +2,8 @@
 
 namespace Pim\Bundle\ExtendedMeasureBundle\Mapper;
 
-use Pim\Bundle\ExtendedMeasureBundle\Exception\DuplicateUnitException;
 use Pim\Bundle\ExtendedMeasureBundle\Exception\UnknownUnitException;
+use Pim\Bundle\ExtendedMeasureBundle\Exception\UnresolvableUnitException;
 
 /**
  * Map a measure to a a PIM unit
@@ -28,10 +28,15 @@ class MeasureMapper
     private $unitFamilies;
 
     /**
+     * Unresolvable measures because of a unit in more than one family
+     *
      * @var array
      */
-    private $unresolvableUnits;
+    private $unresolvableMeasures;
 
+    /**
+     * @param array $config
+     */
     public function __construct(array $config)
     {
         $this->config = $config;
@@ -39,17 +44,23 @@ class MeasureMapper
     }
 
     /**
+     * Retrieve a PIM measure from a unit (Hz, Km, ...)
+     *
      * @param string $unit
      *
      * @return array
      */
-    public function getPimUnit($unit)
+    public function getPimMeasure($unit)
     {
-        if (array_key_exists($unit, $this->unresolvableUnits)) {
-            throw new DuplicateUnitException();
+        if (array_key_exists($unit, $this->unresolvableMeasures)) {
+            $message = sprintf('Unable to resolve the unit "%s" in', $unit);
+            foreach ($this->unresolvableMeasures[$unit] as $unresolvables) {
+                $message .= vsprintf(' [family: %s, measure: %s]', $unresolvables);
+            }
+            throw new UnresolvableUnitException($message);
         }
         if (!array_key_exists($unit, $this->unitNames)) {
-            throw new UnknownUnitException();
+            throw new UnknownUnitException($unit);
         }
 
         return [
@@ -66,49 +77,61 @@ class MeasureMapper
     private function buildResolvableUnits($measuresConfig)
     {
         $this->unitNames = [];
-        $this->unresolvableUnits = [];
-        foreach ($measuresConfig as $unitFamily => $units) {
+        $this->unresolvableMeasures = [];
+        foreach ($measuresConfig as $pimFamily => $units) {
             foreach ($units['units'] as $pimUnit => $unitConfig) {
-                $this->parseUnit($unitConfig, $pimUnit, $unitFamily);
+                $this->parseUnit($unitConfig, $pimUnit, $pimFamily);
             }
         }
     }
 
     /**
+     * Parse one measure definition:
+     *      CUBIC_MILLIMETER:
+     *          unece_code: 'MMQ'
+     *          convert: [{'mul': 0.000000001}]
+     *          symbol: 'mm³'
+     *          name: 'cubic millimeter'
+     *          alternative_units: ['foo', 'bar']
+     *
      * @param array  $unitConfig
      * @param string $pimUnitName
-     * @param string $unitFamily
+     * @param string $pimFamily
      */
-    private function parseUnit(array $unitConfig, $pimUnitName, $unitFamily)
+    private function parseUnit(array $unitConfig, $pimUnitName, $pimFamily)
     {
-        $this->resolveUnit($unitConfig['symbol'], $pimUnitName, $unitFamily);
+        $this->resolveUnit($unitConfig['symbol'], $pimUnitName, $pimFamily);
         if (isset($unitConfig['unece_code'])) {
-            $this->resolveUnit($unitConfig['unece_code'], $pimUnitName, $unitFamily);
+            $this->resolveUnit($unitConfig['unece_code'], $pimUnitName, $pimFamily);
         }
         if (isset($unitConfig['alternative_units'])) {
             foreach ($unitConfig['alternative_units'] as $alternativeUnit) {
-                $this->resolveUnit($alternativeUnit, $pimUnitName, $unitFamily);
+                $this->resolveUnit($alternativeUnit, $pimUnitName, $pimFamily);
             }
         }
     }
 
     /**
+     * Identify unresolvable units that are in more than one family.
+     *
      * @param string $unit
      * @param string $pimUnitName
-     * @param string $unitFamily
+     * @param string $pimFamily
      */
-    private function resolveUnit($unit, $pimUnitName, $unitFamily)
+    private function resolveUnit($unit, $pimUnitName, $pimFamily)
     {
-        if (isset($this->unresolvableUnits[$unit])) {
-            return;
-        }
-        if (!isset($this->unitNames[$unit])) {
-            $this->unitNames[$unit] = $pimUnitName;
-            $this->unitFamilies[$unit] = $unitFamily;
-        } else {
+        if (isset($this->unresolvableMeasures[$unit])) {
+            $this->unresolvableMeasures[$unit][] = [$pimFamily, $pimUnitName];
+        } elseif (isset($this->unitFamilies[$unit])) {
+            $this->unresolvableMeasures[$unit] = [
+                [$this->unitFamilies[$unit], $this->unitNames[$unit]],
+                [$pimFamily, $pimUnitName],
+            ];
             unset($this->unitNames[$unit]);
             unset($this->unitFamilies[$unit]);
-            $this->unresolvableUnits[$unit] = 1;
+        } else {
+            $this->unitNames[$unit] = $pimUnitName;
+            $this->unitFamilies[$unit] = $pimFamily;
         }
     }
 }
